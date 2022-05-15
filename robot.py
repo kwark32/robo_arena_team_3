739661adeb48109859e3a138c6b68ee03ffcba71
airzocker <warthundertest@web.de>
2022-05-15 19:09:15 +0200

    Added circle drawing and changed class vars to instance vars

--- robot.py
+++ robot.py
@@ -1,22 +1,21 @@
 from util import Vector, ns_to_s
 import time
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtCore import QPoint
 
 
 class Robot:
-    radius = 15
-    position = Vector(0.0, 0.0)
-    rotation = 0.0
-    velocity = Vector(0.0, 0.0)  # in px/s
-    ang_velocity = 0.0  # in deg/s
-    __last_move_time_ns = 0
-
-    def __init__(self, position=0, rotation=0):
+    def __init__(self, radius=15, position=Vector(0.0, 0.0), rotation=Vector(0.0, 0.0)):
+        self.radius = radius
         self.position = position
         self.rotation = rotation
+        self.velocity = Vector(0.0, 0.0)  # in px/s
+        self.ang_velocity = 0.0  # in deg/s
         self.__last_move_time_ns = time.time_ns()
 
-    def draw(self):
-        print("Drawing robot...")
+    def draw(self, qp):
+        qp.setPen(QColor(200, 200, 200))
+        qp.drawEllipse(QPoint(self.position.x, self.position.y), self.radius, self.radius)
 
     def update(self):
         time_diff = ns_to_s(time.time_ns() - self.__last_move_time_ns)
--- util.py
+++ util.py
@@ -1,7 +1,4 @@
 class Vector:
-    x = 0
-    y = 0
-
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y

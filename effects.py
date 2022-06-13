978332d695bdc87bc8c3138c1ff5de79c308f643
airzocker <warthundertest@web.de>
2022-06-14 01:08:20 +0200

    Refactoring & improving effects

--- effects.py
+++ effects.py
@@ -3,6 +3,7 @@ from constants import FIXED_DELTA_TIME
 
 class RobotEffect:
     def __init__(self, duration):
+        self.effect_class = type(self)
         self.duration = duration
 
     def apply(self, robot, delta_time=0):
@@ -12,55 +13,36 @@ class RobotEffect:
         pass
 
 
-class EarthTileEffect(RobotEffect):
-    slowdown = 60
-    ang_slowdown = 2
-
-    def __init__(self, duration=(FIXED_DELTA_TIME / 2)):
+class SpeedEffect(RobotEffect):
+    def __init__(self, duration=(FIXED_DELTA_TIME / 2)):  # duration: half physics frame (gets applied 1 frame)
         super().__init__(duration)
 
     def apply(self, robot, delta_time=0):
         super().apply(robot, delta_time=delta_time)
 
-        robot.sim_body.max_velocity -= EarthTileEffect.slowdown
-        robot.sim_body.max_ang_velocity -= EarthTileEffect.ang_slowdown
+        robot.sim_body.max_velocity += self.effect_class.speed_gain
+        robot.sim_body.max_ang_velocity += self.effect_class.ang_speed_gain
 
     def revert(self, robot):
         robot.sim_body.max_velocity = robot.max_velocity
         robot.sim_body.max_ang_velocity = robot.max_ang_velocity
 
 
-class WaterTileEffect(RobotEffect):
-    slowdown = 90
-    ang_slowdown = 3
-
-    def __init__(self, duration=(FIXED_DELTA_TIME / 2)):
-        super().__init__(duration)
-
-    def apply(self, robot, delta_time=0):
-        super().apply(robot, delta_time=delta_time)
-
-        robot.sim_body.max_velocity -= WaterTileEffect.slowdown
-        robot.sim_body.max_ang_velocity -= WaterTileEffect.ang_slowdown
-
-    def revert(self, robot):
-        robot.sim_body.max_velocity = robot.max_velocity
-        robot.sim_body.max_ang_velocity = robot.max_ang_velocity
+class StunEffect(SpeedEffect):
+    speed_gain = -1000000
+    ang_speed_gain = -1000000
 
 
-class FireTileEffect(RobotEffect):
-    slowdown = -60
-    ang_slowdown = -2
+class EarthTileEffect(SpeedEffect):
+    speed_gain = -60
+    ang_speed_gain = -2
 
-    def __init__(self, duration=(FIXED_DELTA_TIME / 2)):
-        super().__init__(duration)
 
-    def apply(self, robot, delta_time=0):
-        super().apply(robot, delta_time=delta_time)
+class WaterTileEffect(SpeedEffect):
+    speed_gain = -90
+    ang_speed_gain = -3
 
-        robot.sim_body.max_velocity -= FireTileEffect.slowdown
-        robot.sim_body.max_ang_velocity -= FireTileEffect.ang_slowdown
 
-    def revert(self, robot):
-        robot.sim_body.max_velocity = robot.max_velocity
-        robot.sim_body.max_ang_velocity = robot.max_ang_velocity
+class FireTileEffect(SpeedEffect):
+    speed_gain = 60
+    ang_speed_gain = 2

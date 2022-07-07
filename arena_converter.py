0dfdf47403c30c8a70ebf0162e05c9fbfa8c7b6d
Kwark32 <lautemannmatts@gmail.com>
2022-07-07 15:57:34 +0200

    Added png-to-arena-converter

--- arena_converter.py
+++ arena_converter.py
@@ -4,11 +4,12 @@ except ImportError:
     import json
 
 from arena import Arena, tile_type_dict
-from util import Vector
+from util import Vector, get_main_path
 from constants import ARENA_SIZE, MAP_FORMAT_VERSION
+from PIL import Image
 
 
-def load_map(file, size, physics_world=None):
+def load_map_json(file, size, physics_world=None):
     map_text = open(file, "r").read()
     map_json = json.loads(map_text)
     if map_json["version"] != MAP_FORMAT_VERSION:
@@ -53,5 +54,49 @@ def load_map(file, size, physics_world=None):
                     physics_world.add_rect(Vector(x, ARENA_SIZE - y), width, height, user_data=tile_type)
                 t += tile["count"]
             r += row["count"]
+
+    return arena
+
+
+tile_type_colors = {
+    (150, 150, 150): "ground",
+    (0, 0, 0): "wall",
+    (150, 100, 0): "earth",
+    (0, 255, 0): "tower_1",
+    (255, 255, 255): "hole",
+    (0, 0, 255): "water",
+    (255, 0, 0): "lava",
+    (255, 100, 0): "fire",
+    (255, 0, 255): "portal_1",
+    (0, 255, 255): "portal_2"
+}
+
+
+def load_map_png(file, size, physics_world=None):
+    im = Image.open(file)
+    pix = im.load()
+
+    if im.size[0] != im.size[1]:
+        print("ERROR: Image not square!")
+        return None
+
+    arena = Arena(size, im.size[0])
+    tiles = arena.get_empty_tiles()
+
+    for y in range(im.size[1]):
+        for x in range(im.size[0]):
+            tile_type = tile_type_colors.get(pix[x, y])
+            if tile_type is None:
+                continue
+            tiles[y][x] = tile_type_dict[tile_type]
+            if physics_world is not None:
+                if tiles[y][x].has_collision:
+                    width = arena.tile_size
+                    height = arena.tile_size
+                    x_pos = x * arena.tile_size + int(width / 2)
+                    y_pos = y * arena.tile_size + int(height / 2)
+                    physics_world.add_rect(Vector(x_pos, ARENA_SIZE - y_pos), width, height, user_data=tile_type)
+
+    arena.tiles = tiles
 
     return arena
